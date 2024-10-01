2e0a472c3a11c4102338097d10c426bbf1ab711f
Chris Gillis <chris85gillis@gmail.com>
2024-10-01 18:39:13 -0500

    changed all a's to link's

--- components/Topic.tsx
+++ components/Topic.tsx
@@ -1,4 +1,6 @@
 import { HashtagIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
 
 type Props = {
   id: string;
@@ -7,7 +9,7 @@ type Props = {
 
 export function Topic({ id, text }: Props) {
   return (
-    <a
+    <link
       href={`/ui/topics/${id}`}
       className="flex items-center border-l border-r border-t border-atlas-white-300 p-6 first:rounded-t-md last:rounded-b-md last:border-b"
     >
@@ -15,6 +17,6 @@ export function Topic({ id, text }: Props) {
         <HashtagIcon className="h-6 w-6 mr-2" />
         {text}
       </p>
-    </a>
+    </link>
   );
 }
