df760057faeab06f681181bcab283101ee10abd4
Chris Gillis <chris85gillis@gmail.com>
2024-10-02 13:57:13 -0500

    Task 4, added server actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { insertTopic } from "./data";
 import { redirect } from "next/navigation";
+import { insertQuestion, incrementVotes } from "./data";
 
 export async function addTopic(data: FormData) {
   let topic;
@@ -18,3 +19,27 @@ export async function addTopic(data: FormData) {
     topic && redirect(`/ui/topics/${topic.id}`);
   }
 }
+
+export async function addQuestion(question: FormData) {
+    try {
+      insertQuestion({
+        title: question.get("title") as string,
+        topic_id: question.get("topic_id") as string,
+        votes: 0,
+      });
+      revalidatePath("/ui/topics/[id]", "page");
+    } catch (error) {
+      console.error("Database Error:", error);
+      throw new Error("Failed to add question.");
+    }
+  }
+
+export async function addVote(data: FormData) {
+    try {
+      incrementVotes(data.get("id") as string);
+      revalidatePath("/ui/topics/[id]", "page");
+    } catch (error) {
+      console.error("Database Error:", error);
+      throw new Error("Failed to add vote.");
+    }
+  }
